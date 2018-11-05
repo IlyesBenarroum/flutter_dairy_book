b371087953a73d58366d4b55724974248ad1c560
Mir Mahfuz <mirmahfuz99@gmail.com>
2018-11-05 13:57:06 +0600

    sticky header initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dairy_book/bottombar/appbar_bottombar.dart';
 import 'package:flutter_dairy_book/dialog_pop_demo/dialog_form_home.dart';
+import 'package:flutter_dairy_book/sticky_headers/sticky_headers.dart';
 import 'package:flutter_dairy_book/warp_demo/warp_demo.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new ExampleApp());
 
 class MyApp extends StatelessWidget {
 
