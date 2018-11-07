e8f65bad9959891b88464fc82deac79399812954
Mir Mahfuz <mirmahfuz99@gmail.com>
2018-11-07 12:17:21 +0600

    model added

---
+++ lib/movie_details_page/models.dart
@@ -0,0 +1,39 @@
+class Movie {
+
+  Movie({
+    this.bannerUrl,
+    this.posterUrl,
+    this.title,
+    this.rating,
+    this.starRating,
+    this.categories,
+    this.storyline,
+    this.photoUrls,
+    this.actors,
+  });
+  
+  final String bannerUrl;
+  final String posterUrl;
+  final String title;
+  final double rating;
+  final int starRating;
+  final List<String> categories;
+  final String storyline;
+  final List<String> photoUrls;
+  final List<Actor> actors;
+
+}
+
+class Actor {
+
+  final String name;
+  final avatarUrl;
+
+  Actor({
+    this.name,
+    this.avatarUrl,
+  });
+
+}
+
+
