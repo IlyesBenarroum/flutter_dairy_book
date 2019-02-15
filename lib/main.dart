bdbe70cd08f694024f79b44b7c58a6562d1ae3bb
Mir Mahfuz <mirmahfuz99@gmail.com>
2019-02-15 12:05:43 +0600

    DropdownMenu Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dairy_book/bottombar/appbar_bottombar.dart';
 import 'package:flutter_dairy_book/bottombar/bottomnavigationhangabove.dart';
 import 'package:flutter_dairy_book/dialog_pop_demo/dialog_form_home.dart';
+import 'package:flutter_dairy_book/show_dialog_alert_dialog_dropdown_menu/drop_down_menu.dart';
 import 'package:flutter_dairy_book/sticky_headers/sticky_header_with_json.dart';
 import 'package:flutter_dairy_book/sticky_headers/sticky_headers.dart';
 import 'package:flutter_dairy_book/warp_demo/warp_demo.dart';
@@ -14,12 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
 //      home: new AppBarBottom(),
-        home: CompanyPage(title: "Company Page")
+        home: DropdownMenu(),
 
     );
   }
---
+++ lib/show_dialog_alert_dialog_dropdown_menu/drop_down_menu.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class DropdownMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Show Dialog'),
+        backgroundColor: Color(0xff8c3a3a),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: choiceAction,
+            itemBuilder: (BuildContext context) {
+              return Constants.choices.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+
+
+  void choiceAction(String choice) {
+    if (choice == Constants.FirstItem) {
+      print('I First Item');
+    } else if (choice == Constants.SecondItem) {
+      print('I Second Item');
+    } else if (choice == Constants.ThirdItem) {
+      print('I Thired Item');
+    }
+  }
+}
+class Constants{
+  static const String FirstItem = 'First Item';
+  static const String SecondItem = 'Second Item';
+  static const String ThirdItem = 'Third Item';
+
+  static const List<String> choices = <String>[
+    FirstItem,
+    SecondItem,
+    ThirdItem,
+  ];
+
+
+
+
+}
